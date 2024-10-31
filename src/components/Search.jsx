7bb4e0f5473de1a8511547243c03e4567350b57d
WareBar <barryadam615@gmail.com>
2024-10-31 09:37:07 -0800

    Added Search Components for datas

---
+++ src/components/Search.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+const apiUrl = 'https://api.spoonacular.com/recipes/complexSearch'
+const apiKey = '88affbb53dcf4de199c109f524a999e5'
+
+
+const Search = () => {
+// always use effectHooks inside of a component, and never outside
+// useEffects Hooks are used when we want a component to sync with an external API so that whenever components changes, the data changes always
+// useEffect Hooks are similar to JS addEventListener
+// as they both allow you to react to changes or events and trigger specific actions or function
+
+
+    const [query, setQuery] = useState('Pizza');
+    useEffect(()=>{
+        async function fetchFood(){
+            // fetch is a js built-in function that collects a data based on passed url API
+            const res = await fetch(`${apiUrl}?query=${query}&apiKey=${apiKey}`)
+            const data = await res.json()
+            console.log(data)
+        }
+        fetchFood()
+
+    },[query])
+
+    return (
+        <div className="Search">
+            <input type="text" id="searchBox"
+            onChange={(e)=>setQuery(e.target.value)}
+            />
+        </div>
+    )
+}
+
+export default Search;
