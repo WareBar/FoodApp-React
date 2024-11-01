a0a91444ca5f3b72ba89bdea606ddda86acf6973
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-01 04:14:08 +0000

    removed key

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,13 +1,12 @@
 import styles from '../components_styles/foodItem.module.css'
 const FoodItem = ({foodItem}) => {
     return (
-        <div className={styles.FoodItem} key={foodItem.id}>
+        <div className={styles.FoodItem}>
 
             <img src={foodItem.image} alt={foodItem.title} />
             <div className={styles.foodContent}>
                 <p className={styles.foodName}>{foodItem.title}</p>
             </div>
-            {/* <h1>{foodItem.title}</h1> */}
             <button className={styles.foodButton}>View Recipe</button>
         
         
