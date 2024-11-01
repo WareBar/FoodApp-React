592291b572881369da9a4ad6114e363ebb463140
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-01 10:46:25 +0000

    used string concatenation instead of literal string format due to API syntax error

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,19 +1,18 @@
 import { useEffect } from "react";
 
 const FoodDetails = ({foodId}) => {
-    const apiURL = `https://api.spoonacular.com/recipes/${foodId}/information`
     const apiKey = '8724467ba94943a5b49ea9de72e8d1f3'
 
     useEffect(()=>{
         async function fetchFood(){
-            // TODO: FIX THE URL API FOOD ID
-            const res = await fetch(`https://api.spoonacular.com/recipes/655698/information?apiKey=${apiKey}`)
+            // i used this format of string instead of literal string because im having problem in syntax
+            const res = await fetch('https://api.spoonacular.com/recipes/'+foodId+'/information?apiKey='+apiKey)
             const data = res.json()
             console.log(data)
         }
         fetchFood()
 
-    }, [])
+    }, [foodId])
 
 
     return (
