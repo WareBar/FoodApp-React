958d6d03644c94076c102fa449f62d1a13397082
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-01 03:21:12 +0000

    added styling classes

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,11 +1,17 @@
+import styles from '../components_styles/foodItem.module.css'
 const FoodItem = ({foodItem}) => {
     return (
-        <>
+        <div className={styles.FoodItem} key={foodItem.id}>
+
+            <img src={foodItem.image} alt={foodItem.title} />
+            <div className={styles.foodContent}>
+                <p className={styles.foodName}>{foodItem.title}</p>
+            </div>
+            {/* <h1>{foodItem.title}</h1> */}
+            <button className={styles.foodButton}>View Recipe</button>
         
-        <p>{foodItem.title}</p>
-        <p>{foodItem.id}</p>
         
-        </>
+        </div>
     )
 }
 
