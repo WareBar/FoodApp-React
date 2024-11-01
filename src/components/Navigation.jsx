458e05495f0192b326d4a0c4c5bb61e93018a6aa
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-01 03:24:08 +0000

    added Navigation Component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import styles from '../components_styles/nav.module.css'
+
+const Navigation = () => {
+    return (
+        <nav className={styles.Navigation}>
+            <h1>🍔 Food App</h1>
+        </nav>
+    );
+};
+
+
+
+export default Navigation;
