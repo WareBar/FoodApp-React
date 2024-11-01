857d8d8bc65f4a8c437dfffa393a5c692a14ecf8
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-01 05:46:35 +0000

    drilled foodId props

--- src/App.js
+++ src/App.js
@@ -3,26 +3,36 @@ import './App.css';
 import Search from './components/Search';
 import FoodList from './components/FoodList';
 import Navigation from './components/Navigation';
+import FoodDetails from './components/FoodDetails';
 import Container from './components/Container';
-import InnerContainer from './components/innerContainer';
+import InnerContainer from './components/InnerContainer';
 
 function App() {
 
   const [foodData, setFoodData] = useState([])
+  const [foodId, setFoodId] = useState("")
 
   return (
     <div className="App">
       <Navigation/>
       <Search setFoodData={setFoodData}/>
-
-
       <Container>
         <InnerContainer>
-          <FoodList foodData={foodData}/>          
+          <FoodList foodData={foodData} setFoodId={setFoodId}/>
         </InnerContainer>
-      </Container>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+        
+      </Container>   
+
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
