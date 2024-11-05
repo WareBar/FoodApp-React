e379ee91fc24bcd9f443ade5357b2238e8ab8e93
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-05 11:12:17 +0000

    created itemList and its initial setup

---
+++ src/components/itemList.jsx
@@ -0,0 +1,17 @@
+const ItemList = ({food, isLoading}) =>{
+    return (
+        <div>
+            {isLoading? 'Ingredients...':
+            
+            food.extendedIngredients.map((ingredientItem)=>(
+                <div>
+                    <img key={ingredientItem.id} src={`https://img.spoonacular.com/ingredients_100x100/${ingredientItem.image}`} alt="" />
+                    <h3>{ingredientItem.name}</h3>
+                </div>
+            ))
+            }
+        </div>
+    )
+}
+
+export default ItemList;
