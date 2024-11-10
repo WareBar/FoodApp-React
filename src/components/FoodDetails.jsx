b448db73450bce1ffb58a92b1348bf8b047476e6
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-10 12:40:50 +0000

    added FoodDetails Component

---
+++ src/components/FoodDetails.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+const FoodDetails = ({foodId}) =>{
+    const KEY = 'd3f8ee2aa31e4719a0d8e798393c77a5'
+    const [food, setFood] = useState({})
+
+    useEffect(()=>{
+        async function fetchFood(){
+            const result = await fetch('https://api.spoonacular.com/recipes/'+foodId+'/information?apiKey='+KEY)
+            const data = await result.json()
+            console.log(data)
+            setFood(data)
+        }
+
+        fetchFood()
+
+    },[food])
+
+    return (
+        <div>
+            <p>THIS IS FOOD RECIPE COMPONENT</p>
+            <h1>{food.title}</h1>
+        </div>
+    )
+}
+
+export default FoodDetails;
