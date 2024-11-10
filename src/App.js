c14cd60b43dceb188a34c99440ba28654db388e5
Abdul Barry A. Adam <barryadam615@gmail.com>
2024-11-10 12:42:22 +0000

    added router navigation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import { useState } from 'react';
 import Navigation from './components/Navigation';
 import Search from './components/Search';
-
+import FoodList from './components/FoodList'
+import FoodDetails from './components/FoodDetails';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
@@ -13,6 +15,12 @@ function App() {
     <div className="App">
       <Navigation/>
       <Search setFoodData={setFoodData}/>
+
+      <Routes>
+        <Route index element={
+          <FoodList foodData={foodData} setFoodId={setFoodId} />}/>
+        <Route path="/Recipe" element={<FoodDetails foodId={foodId}/>}/>
+      </Routes>
 
     </div>
   );
