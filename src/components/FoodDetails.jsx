98573fd57b09a1fbace0b995de9a1efdf70260d2
WareBar <barryadam615@gmail.com>
2024-12-22 19:49:32 +0800

    updates meta info

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock, faUtensils, faAppleAlt } from '@fortawesome/free-solid-svg-icons';
 
 const FoodDetails = ({foodId}) =>{
-    const KEY = '8f01c50cdb9f46b6bdb00a2bdf5d65a5'
+    const KEY = '3ae7d9802f9940ad87b57e95c77ceb60'
     const [food, setFood] = useState({})
     const [loading, isLoading] =useState(true);
 
@@ -34,8 +34,8 @@ const FoodDetails = ({foodId}) =>{
                     <div className={styles.meta_info}>
                         <p>
                         
-                            <span><FontAwesomeIcon className="fontIcon" icon={faClock}/>: {food.readyInMinutes}</span>
-                            <span><FontAwesomeIcon className="fonticon" icon={faUtensils}/>: {food.servings}</span>
+                            <span><FontAwesomeIcon className="fontIcon" icon={faClock}/>: {food.readyInMinutes} minutes</span>
+                            <span><FontAwesomeIcon className="fonticon" icon={faUtensils}/>: {food.servings} servings</span>
                             <span><FontAwesomeIcon className="fonticon" icon={faAppleAlt}/>: {food.vegetarian? 'Vegetarian':'Non-Vegetarian'}</span>
                             {/* <span>{food.vegetarian? 'VEGE':'NOT VEGE'}</span> */}
                             <span>${food.pricePerServing} per serving</span>
@@ -55,7 +55,7 @@ const FoodDetails = ({foodId}) =>{
                                 <img src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`} alt={item.name} />
                                 <p className={styles.IngredientsName}>
                                     {item.name}
-                                    <span>{item.original}</span>
+                                    <span className={styles.IngredientsOrig}>{item.original}</span>
                                 </p>
                             </div>
                         ))
